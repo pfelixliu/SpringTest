dcde4a808c717772f77b8caef804441286c5c7de
felixliu <p_pfelixliu@tencent.com>
2020-08-04 18:41:32 +0800

    测试Git

---
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,7 @@
+package com.example.demo.controller;
+
+/**
+ * @author felixliu
+ */
+public class UserController {
+}
---
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,7 @@
+package com.example.demo.service;
+
+/**
+ * @author felixliu
+ */
+public interface UserService {
+}
